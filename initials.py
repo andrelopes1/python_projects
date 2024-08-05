35369fc94c928fbbac0a612b7da8b157129a8022
André Lopes <65914110+andrelopes1@users.noreply.github.com>
2024-08-05 12:33:17 +0100

    initials.py project

---
+++ initials.py
@@ -0,0 +1,22 @@
+# ANDRE LOPES
+# A fun fact about myself is that I enjoy coding and learning new programming languages.
+
+# Block letter A
+print("  A  ")
+print(" A A ")
+print("A   A")
+print("AAAAA")
+print("A   A")
+print("A   A")
+print("A   A")
+
+print()  # Print a blank line between initials
+
+# Block letter L
+print("L    ")
+print("L    ")
+print("L    ")
+print("L    ")
+print("L    ")
+print("L    ")
+print("LLLLL")
