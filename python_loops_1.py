5a341d212e2994f9bd60fe21a720aa9a2887a5c0
André Lopes <65914110+andrelopes1@users.noreply.github.com>
2024-08-09 12:43:29 +0100

    Python loops

---
+++ python_loops_1.py
@@ -0,0 +1,9 @@
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+# Your code below:
+index = 0
+length = len(python_topics)
+
+while index < length:  # Change condition to index < length
+  print("I am learning about " + python_topics[index])
+  index += 1
