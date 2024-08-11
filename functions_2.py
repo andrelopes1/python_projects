7a7e3e52e2d67933a491eed90db9908bc35d52bd
André Lopes <65914110+andrelopes1@users.noreply.github.com>
2024-08-11 23:02:45 +0100

    Create funtions_2.py

---
+++ functions_2.py
@@ -0,0 +1,8 @@
+# Your code below:
+
+def generate_trip_instructions(location):
+  print("Looks like you are planning a trip to visit " + location)
+  print("You can use the public subway system to get to " + location)
+
+
+generate_trip_instructions("Grand Central Station")
