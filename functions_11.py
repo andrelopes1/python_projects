53ddbaad62c9319e7f34697af42396a99d97d18c
André Lopes <65914110+andrelopes1@users.noreply.github.com>
2024-08-13 14:16:02 +0100

    Create functions_11.py

---
+++ functions_11.py
@@ -0,0 +1,12 @@
+company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
+
+
+second_to_last = company_motto[-2:-1]
+
+
+final_word = company_motto[-4:]
+
+
+
+
+print(final_word)
