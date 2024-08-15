b08c6e1b1d966f63999ce35e4203f692350d915c
André Lopes <65914110+andrelopes1@users.noreply.github.com>
2024-08-15 17:56:42 +0100

    Create modules_1.py

---
+++ modules_1.py
@@ -0,0 +1,19 @@
+# Import random below:
+
+import random 
+
+# Create random_list below:
+
+random_list = []
+
+# Create randomer_number below:
+
+random_list = [random.randint(1, 100) for _ in range(101)]
+
+
+# Print randomer_number below:
+
+randomer_number = random.choice(random_list)
+
+
+print(randomer_number)
