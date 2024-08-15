b6d262ab0eb59252137c77ffa0584ed94da9376e
André Lopes <65914110+andrelopes1@users.noreply.github.com>
2024-08-15 21:41:57 +0100

    Create dictionary_6.py

---
+++ dictionary_6.py
@@ -0,0 +1,13 @@
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+
+
+
+oscar_winners.update({"Supporting Actress": "Viola Davis"})
+
+
+oscar_winners["Best Picture"] =  "Moonlight"
+
+
+
+
+print(oscar_winners)
