a6dd5a453ff72ba0247cbd859c88bd466a2a87a6
André Lopes <65914110+andrelopes1@users.noreply.github.com>
2024-08-16 22:30:42 +0100

    py

---
+++ learn_2.py
@@ -0,0 +1,6 @@
+with open('just_the_first.txt') as first_line_doc:
+    # Step 2: Save the first line of just_the_first.txt into the variable first_line
+    first_line = first_line_doc.readline()
+
+    # Step 3: Print out the variable first_line
+    print(first_line)
---
+++ learn_4.py
@@ -0,0 +1,3 @@
+with open('cool_dogs.txt', 'a') as cool_dogs_file:
+    # Step 2: Add "Air Buddy\n" to the file
+    cool_dogs_file.write("Air Buddy\n")
